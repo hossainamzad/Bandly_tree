a3317a83c57963ea820ce414edfbd594029792ea
Matthew Solarski <mxtthxw@gmail.com>
2016-05-29 22:02:50 -0400

    Moved Allmusic class to lib, added logic to home controller index, added code for four influencers to home index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
+require 'allmusic'
+
 class HomeController < ApplicationController
   def index
-  	
+  	# temporarily hardcoding artist name for now -- once search is set up this will change to params:
+  	@artist = Allmusic.new("interpol")
+  	@artist_amg_page = @artist.return_artist_url + "/related"
+  	@artist_amg_page_object = Nokogiri::HTML(open(@artist_amg_page))
+  	# hardcoded version of Nokogiri parse:
+  	# @artist_amg_page_object = Nokogiri::HTML(open("http://www.allmusic.com/artist/interpol-mn0000096576/related"))
   end
 end
